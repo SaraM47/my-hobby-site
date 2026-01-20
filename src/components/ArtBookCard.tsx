ce716edca29ea46e6d41222bbb1f6ee9cf897ee0
SaraMsoso <Sara.soder44@gmail.com>
2026-01-20 22:30:56 +0100

    Added som inline-CSS, improved image alt text for accessibility and removed redundancy

--- src/components/ArtBookCard.tsx
+++ src/components/ArtBookCard.tsx
@@ -9,14 +9,25 @@ export const ArtBookCard = ({ book }: ArtBookCardProps) => {
   return (
     <article className="bookCard">
       {/* Book cover image */}
-      <img src={book.image} alt={book.title} className="bookImage" />
+      <img
+        src={book.image}
+        alt={`Omslag till ${book.title}`}
+        className="bookImage"
+      />
       <h2>{book.title}</h2>
 
       <p>Author: {book.author}</p>
       <p>Year of release: {book.releaseYear}</p>
 
       {/* Conditional operation rendering to display if the book has been read */}
-      <p>Read: {book.isRead ? "Yes" : "No"}</p>
+      <p
+        style={{
+          fontWeight: book.isRead ? "bold" : "bold",
+          color: book.isRead ? "green" : "#842029",
+        }}
+      >
+        Läst: {book.isRead ? "Ja" : "Nej"}
+      </p>
     </article>
   );
 };
