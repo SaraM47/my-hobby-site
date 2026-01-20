f975ffd0b06ecb12381c877450df1d897b2c68aa
SaraMsoso <Sara.soder44@gmail.com>
2026-01-20 21:59:50 +0100

    Added image property to ArtBook data and include cover images

--- src/data/artbooks.ts
+++ src/data/artbooks.ts
@@ -1,10 +1,20 @@
-// Definition of the Artbook interface for artbooks collection with id, title, author, releaseYear, and isRead properties
+import weiboImg from "../assets/artbooks/weibo.png"
+import violaImg from "../assets/artbooks/viola.png"
+import hinataImg from "../assets/artbooks/hinata.png"
+import naranoImg from "../assets/artbooks/narano.png"
+import buchiImg from "../assets/artbooks/buchi.png"
+import ekuImg from "../assets/artbooks/eku.png"
+import waccaImg from "../assets/artbooks/wacca.png"
+import kazunokoImg from "../assets/artbooks/kazunoko.png"
+
+// Definition of the Artbook interface for artbooks collection with id, title, author, releaseYear, isRead and image properties
 export interface ArtBook { 
     id : number, // integer
     title : string, // string of title of the book
     author : string, // string of author of the book
     releaseYear : number, // integer of release year
     isRead : boolean, // boolean indicating if the book has been read
+    image : string // string of image URL
 }
 
 // Sample data for artbooks collection with 6 entries 
@@ -15,6 +25,7 @@ export const artbooks : ArtBook[] = [
         author: "WEIBO",
         releaseYear: 2024,
         isRead: true,
+        image: weiboImg,
     },
     {
         id: 2,
@@ -22,13 +33,15 @@ export const artbooks : ArtBook[] = [
         author: "Senbon Umishima",
         releaseYear: 2024,
         isRead: false,
+        image: violaImg,
     },
     {
         id: 3,
         title: "Singing with the Seasons, Dancing with the Flowers - Hinata Art Collection",
-        author: "Hinato",
+        author: "Hinata",
         releaseYear: 2024,
         isRead: true,
+        image: hinataImg,
     },
     {
         id: 4,
@@ -36,13 +49,15 @@ export const artbooks : ArtBook[] = [
         author: "Narano",
         releaseYear: 2024,
         isRead: true,
+        image: naranoImg,
     },
     {
         id: 5,
-        title: "Birthdat – Buchi ArtWorks",
+        title: "Birthday – Buchi ArtWorks",
         author: "Buchi",
         releaseYear: 2024,
         isRead: false,
+        image: buchiImg,
     },
     {
         id: 6,
@@ -50,5 +65,22 @@ export const artbooks : ArtBook[] = [
         author: "Eku Uekura",
         releaseYear: 2023,
         isRead: false,
+        image: ekuImg,
+    },
+    {
+        id: 7,
+        title: "wacca works – Oyogu yume wo mita (I dreamed of swimming",
+        author: "wacca",
+        releaseYear: 2023,
+        isRead: true,
+        image: waccaImg,
+    },
+    {
+        id: 8,
+        title: "Aoboshi",
+        author: "Kazunoko",
+        releaseYear: 2023,
+        isRead: true,
+        image: kazunokoImg,
     }
 ]
