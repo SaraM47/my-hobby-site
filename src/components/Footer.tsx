e6a599dec591f64c1a9c015c112a1c529680505d
SaraMsoso <Sara.soder44@gmail.com>
2026-01-20 14:12:44 +0100

    Crested a footer component with responsive layout and component in css

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import "./Footer.css";
+
+export const Footer = () => {
+    return (
+        <footer className="footer">
+            <p> 2026 Art Books Collection.</p>
+        </footer>
+    )
+}
