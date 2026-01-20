435beabab43ad7adb209ce6e675f5f7b13db1d22
SaraMsoso <Sara.soder44@gmail.com>
2026-01-20 22:01:55 +0100

    Adjused the text in footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import "./Footer.css";
 export const Footer = () => {
     return (
         <footer className="footer">
-            <p> My Art Books Collection, 2026</p>
+            <p> My Art Books Collection, 2026. All images from Japanese Creative Books store</p>
         </footer>
     )
 }
